00eb080cf0452715c28c0191185cfc8d389c68e2
Ian Seyler <ian@seyler.me>
2026-07-26 11:53:36 -0400

    Parameterize script to accept program file as input

--- test.sh
+++ test.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -e
 
+if [ -z "$1" ]; then
+	echo "Usage: $0 <program.c>"
+	exit 1
+fi
+
+PROG_C=$1
+PROG_APP=${PROG_C%.c}.app
+
 cd BareMetal-AppPort
 ./clean.sh
 ./setup.sh
-./build-app.sh https_crawler.c
-cp https_crawler.app ../BareMetal-Firecracker/sys
+./build-app.sh "$PROG_C"
+cp "$PROG_APP" ../BareMetal-Firecracker/sys
 cd ..
 
 cd BareMetal-Firecracker
-./build.sh https_crawler.app
+./build.sh "$PROG_APP"
 ./baremetal.sh start
 sleep 15
 ./baremetal.sh output --full
