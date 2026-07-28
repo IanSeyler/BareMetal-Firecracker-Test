8f4c1dd5aeb9e9ce8363865dfa8c816a63e9bdc7
Ian Seyler <ian.seyler@returninfinity.com>
2026-07-27 20:11:31 -0400

    Add disk image creation to setup

--- setup.sh
+++ setup.sh
@@ -5,3 +5,12 @@ git clone https://github.com/ReturnInfinity/BareMetal-AppPort
 git clone https://github.com/ReturnInfinity/BareMetal-Firecracker
 mkdir BareMetal-AppPort/build/
 cp files/* BareMetal-AppPort/build
+
+DISK="$PWD/disk.img"
+DISKSIZE=512M
+
+# Create the disk image if it doesn't already exist
+if [ ! -f "$DISK" ]; then
+	echo "Creating $DISKSIZE ext2 disk image at $DISK"
+	mkfs.ext2 -q -F "$DISK" "$DISKSIZE"
+fi
