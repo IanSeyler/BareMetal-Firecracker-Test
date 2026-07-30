072765a8854a19069694187ecddc9c70f1026be3
Ian Seyler <ian.seyler@returninfinity.com>
2026-07-30 12:09:07 -0400

    pre-flight checks

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+# Pre-flight checks: make sure required utilities are installed
+for cmd in git mkfs.ext2 curl unzip tar gcc nasm make patch; do
+	if ! command -v "$cmd" > /dev/null 2>&1; then
+		echo "Error: required command '$cmd' not found. Please install it before running this script." >&2
+		exit 1
+	fi
+done
+
 git clone https://github.com/ReturnInfinity/BareMetal-AppPort
 git clone https://github.com/ReturnInfinity/BareMetal-Firecracker
 mkdir BareMetal-AppPort/build/
